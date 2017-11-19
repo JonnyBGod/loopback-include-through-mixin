e07cf56a6e4fdcd9e1f24fea54f0cd836929c64b
Jura Boonnom <jura@ekoapp.com>
2017-11-19 20:26:15 +0700

    - add support for find by id - checkpoint, no lint yet

--- include-through.js
+++ include-through.js
@@ -1,82 +1,163 @@
 'use strict';
 
-module.exports = function(Model, options) {
-  Model.on('attached', function() {
-    var relations = Model.settings.relations || Model.relations;
+const _ = require('lodash');
 
-    if (relations) {
-      Object.keys(relations).forEach(function(targetModel) {
+module.exports = function(Model, options) {
+  var modelHasManyThroughRelations, modelRelations;
+
+  Model.on('attached', function() {
+    modelRelations = Model.settings.relations || Model.relations;
+
+    if (modelRelations) {
+      modelHasManyThroughRelations = [];   
+      Object.keys(modelRelations).forEach(function(targetModel) {
         var type =
-          (relations[targetModel].modelThrough || relations[targetModel].through) ?
-            'hasManyThrough' : relations[targetModel].type;
+          (modelRelations[targetModel].modelThrough || modelRelations[targetModel].through) ?
+            'hasManyThrough' : modelRelations[targetModel].type;
 
         if (type === 'hasManyThrough') {
-          Model.afterRemote('prototype.__get__' + targetModel, injectIncludes);
-          Model.afterRemote('prototype.__create__' + targetModel, injectIncludes);
+          Model.afterRemote('prototype.__get__' + targetModel, controller);
+          Model.afterRemote('prototype.__create__' + targetModel, controller);
+          modelHasManyThroughRelations.push(targetModel);
         }
       });
+
+      if(modelHasManyThroughRelations.length) {
+        Model.afterRemote('findById', controller);
+      }
     }
   });
 
-  function injectIncludes(ctx, unused, next) {
-    if (!ctx.result) return next();
+  function controller(ctx, unused, next) {
+    if (ctx.methodString.indexOf('prototype.__get__') !== -1) {
 
-    var relationName = ctx.methodString.match(/__([a-z\d]+)$/)[1];
-
-    if (
-      !(options.relations && options.relations.indexOf(relationName) !== -1) &&
-      !(ctx.args.filter && ctx.args.filter.includeThrough)
-    ) return next();
-
-    var relationKey = Model.relations[relationName].keyTo;
-    var throughKey = Model.relations[relationName].keyThrough;
-    var relationModel = Model.relations[relationName].modelTo;
-    var throughModel = Model.relations[relationName].modelThrough;
-    var idName = relationModel.definition.idName() || 'id';
-
-    var newResult = JSON.parse(JSON.stringify(ctx.result));
-
-    var query = {where: {}};
-    query.where[relationKey] = ctx.instance.id;
-
-    if (Array.isArray(newResult)) {
-      query.where[throughKey] = {inq: newResult.map(function(item) { return item[idName]; })};
-    } else {
-      query.where[throughKey] = {inq: [newResult[idName]]};
-    }
-
-    if (
-      ctx.args.filter &&
-      ctx.args.filter.includeThrough &&
-      ctx.args.filter.includeThrough.fields
-    ) {
-      query.fields = [throughKey, ctx.args.filter.includeThrough.fields];
-    } else if (options.fields && options.fields[relationName]) {
-      query.fields = [throughKey, options.fields[relationName]];
-    }
-
-    throughModel.find(query, function(err, results) {
-      if (err) return next();
-
-      var resultsHash = {};
-      results.forEach(function(result) {
-        resultsHash[result[throughKey].toString()] = result;
+      // the original version
+      var relationName = ctx.methodString.match(/__([a-z\d]+)$/)[1];    
+      var partialResult = JSON.parse(JSON.stringify(ctx.result));  
+      injectIncludes(ctx, partialResult, relationName).then(function (partialResult) {
+        ctx.result = partialResult;
+        next();
       });
+    } else {
+      // extension
+      var newResult = JSON.parse(JSON.stringify(ctx.result));
+      
+      var filter = ctx.req && ctx.req.query && ctx.req.query.filter;
+      if (filter) {
+        if(typeof filter === 'string') {
+          filter = JSON.parse(filter);
+        }
+        var include = filter.include; // string, [string], object
 
-      if (Array.isArray(newResult)) {
-        for (var i = 0; i < newResult.length; i++) {
-          if (resultsHash[newResult[i][idName].toString()]) {
-            newResult[i][throughModel.definition.name] =
-              resultsHash[newResult[i][idName].toString()];
+        // only support one level of includes
+        var relationNames = [];
+        if (_.isString(include)) {
+          relationNames.push(include)
+        } else if (_.isArray(include)) {
+          for (let elm of include) {
+            if (typeof elm === 'string') {
+              relationNames.push(elm);
+            } else if (typeof elm === 'object') {
+              for (let prop in elm) {
+                relationNames.push(prop);
+              }
+            }
+          }
+        } else if (_.isObject(include)) {
+          for (let prop in include) {
+            relationNames.push(prop);
           }
         }
+        relationNames = _.uniq(relationNames);
+        
+        let promises = [];
+        for (let relationName of relationNames) {
+          if (modelHasManyThroughRelations.includes(relationName)) {
+            let partialResult = newResult[relationName];
+            let promise = injectIncludes(ctx, partialResult, relationName).then(function(partialResult) {
+              return new Promise(function(res, rej) {
+                newResult[relationName] = partialResult;
+                res();
+              });
+            });
+            promises.push(promise);
+          }
+        }
+        if (promises.length) {
+          Promise.all(promises).then(function() {
+            ctx.result = newResult;
+            next();
+          })
+        } else {
+          next();
+        }
+      }
+      else {
+        next();
+      }
+    }
+  }
+
+  function injectIncludes(ctx, partialResult, relationName) {
+    return new Promise(function(res, rej) {
+      if (
+        !(options.relations && options.relations.indexOf(relationName) !== -1) &&
+        !(ctx.args.filter && ctx.args.filter.includeThrough)
+      ) res(partialResult);
+
+      var relationKey = modelRelations[relationName].keyTo;
+      var throughKey = modelRelations[relationName].keyThrough;
+      var relationModel = modelRelations[relationName].modelTo;
+      var throughModel = modelRelations[relationName].modelThrough;
+      var idName = relationModel.definition.idName() || 'id';
+      
+      var query = {where: {}};
+      if (ctx.instance) {
+        query.where[relationKey] = ctx.instance.id;
       } else {
-        newResult[throughModel.definition.name] = resultsHash[newResult[idName].toString()];
+      
+        query.where[relationKey] = ctx.args.id;        
+      }
+      
+      if (Array.isArray(partialResult)) {
+        query.where[throughKey] = {inq: partialResult.map(function(item) { return item[idName]; })};
+      } else {
+        query.where[throughKey] = {inq: [partialResult[idName]]};
       }
 
-      ctx.result = newResult;
+      if (
+        ctx.args.filter &&
+        ctx.args.filter.includeThrough &&
+        ctx.args.filter.includeThrough.fields
+      ) {
+        query.fields = [throughKey, ctx.args.filter.includeThrough.fields];
+      } else if (options.fields && options.fields[relationName]) {
+        query.fields = [throughKey, options.fields[relationName]];
+      }
+      
+      throughModel.find(query, function(err, results) {
+        if (err) res(partialResult);
+        else {
+          var resultsHash = {};
+          results.forEach(function(result) {
+            resultsHash[result[throughKey].toString()] = result;
+          });
 
-      next();
+          if (Array.isArray(partialResult)) {
+            for (var i = 0; i < partialResult.length; i++) {
+              if (resultsHash[partialResult[i][idName].toString()]) {
+                partialResult[i][throughModel.definition.name] =
+                  resultsHash[partialResult[i][idName].toString()];
+              }
+            }
+          } else {
+            partialResult[throughModel.definition.name] =
+              resultsHash[partialResult[idName].toString()];
+          }
+          res(partialResult);
+        }
+      });
     });
+
   };
 };
